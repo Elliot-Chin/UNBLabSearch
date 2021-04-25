a1142ab88b9ba774c0f4b843d696099aedeb1b7a
Elliot <ychin@unb.ca>
2021-04-25 11:08:39 -0300

    added comments to each function

--- src/labSearch/MasterProcessor.java
+++ src/labSearch/MasterProcessor.java
@@ -11,11 +11,24 @@ public class MasterProcessor {
 	private List<Software> swList;
 	private List<Lab> labList;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param swList  - a list of software
+	 * @param labList - a list of labs
+	 */
 	public MasterProcessor(List<Software> swList, List<Lab> labList) {
 		this.swList = swList;
 		this.labList = labList;
 	}
 
+	/**
+	 * searches through each software and return the list of labs that the software
+	 * is in
+	 * 
+	 * @param SoftwareName - the software to search
+	 * @return the list of labs that the software is in
+	 */
 	private List<Lab> searchLab(String SoftwareName) {
 		for (Software sw : swList) {
 			if (sw.getName().equalsIgnoreCase(SoftwareName)) {
@@ -25,6 +38,12 @@ public class MasterProcessor {
 		return null;
 	}
 
+	/**
+	 * searches through each lab and returns the list of software that the lab has
+	 * 
+	 * @param labName - the lab name to search
+	 * @return the list of software that is in the lab
+	 */
 	private List<Software> searchSoftware(String labName) {
 		List<Software> toReturn = new ArrayList<>();
 		for (Software s : swList) {
@@ -36,10 +55,22 @@ public class MasterProcessor {
 		return toReturn;
 	}
 
+	/**
+	 * returns the size of the lab or software list
+	 * 
+	 * @param sizeOf - the type of list size to return
+	 * @return the size of list
+	 */
 	public int getSize(String sizeOf) {
 		return sizeOf.equalsIgnoreCase("Lab") ? labList.size() : swList.size();
 	}
 
+	/**
+	 * uses both searchSoftware and searchLab to get a resultList and return it
+	 * 
+	 * @param query - the query to search (lab name / software name)
+	 * @return the list of results (list of lab / software)
+	 */
 	public List<?> search(String query) {
 		List<?> searchResults;
 		searchResults = searchLab(query);
@@ -53,6 +84,12 @@ public class MasterProcessor {
 		return searchResults;
 	}
 
+	/**
+	 * returns the unique list of labs that contains software. LabList contains labs
+	 * that does not have software in it yet.
+	 * 
+	 * @return the list of labs that contains at least 1 software in it
+	 */
 	public Set<String> getLabListFrmSW() {
 		Set<String> toReturn = new HashSet<>();
 		for (Software s : swList) {
@@ -63,14 +100,30 @@ public class MasterProcessor {
 		return toReturn;
 	}
 
+	/**
+	 * returns the swList
+	 * 
+	 * @return swList
+	 */
 	public List<Software> getSWList() {
 		return swList;
 	}
 
+	/**
+	 * returns labList
+	 * 
+	 * @return labList
+	 */
 	public List<Lab> getLabList() {
 		return labList;
 	}
 
+	/**
+	 * adding a software / lab to the list and updating the file
+	 * 
+	 * @param type - the object itself (lab / software)
+	 * @throws IOException
+	 */
 	public void add(Object type) throws IOException {
 		if (type instanceof Lab) {
 			labList.add((Lab) type);
@@ -83,6 +136,13 @@ public class MasterProcessor {
 		}
 	}
 
+	/**
+	 * removes a software / lab from the list and updating the file
+	 * 
+	 * @param itemName - the item to remove (lab name / software name)
+	 * @param type     - the type of object (lab / software)
+	 * @throws IOException
+	 */
 	public void remove(String itemName, String type) throws IOException {
 		int labListSize = labList.size();
 		int swListSize = swList.size();
